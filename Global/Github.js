2d79307ee0a04bcc18a1548a5ab294c8a2c12a4b
Nick <nicholas.j.campbell@gmail.com>
2009-05-31 01:00:32 -0400

    Issues and access to users.

--- Global/Github.js
+++ Global/Github.js
@@ -2,16 +2,25 @@ if (!global.axiom) {
     global.axiom = {};
 }
 
-axiom.Github = function(username, password, format) {
+axiom.Github = function(username, token, format, repository) {
+    /* VARS */
+    this.STATES = {
+	OPEN: 'open',
+	UNREAD: 'unread',
+	CLOSED: 'closed'
+    };
+
+    /* INIT */
     this.username = username;
-    this.password = password;
+    this.password = token;
     this.format = format || 'json';
+    this.repository = repository;
 
     this.getUsername = function() {
 	return this.username;
     };
 
-    this.getPassword = function() {
+    this.getToken = function() {
 	return this.password;
     };
 
@@ -23,25 +32,108 @@ axiom.Github = function(username, password, format) {
 	return this.format;
     };
 
-    this.Repository = function(name) {
-	this.repository = name;
+    this.setRepository = function(repository) {
+	this.repository = repository;
+    };
 
-	this.getRepository = function() {
-	    return this.repository;
+    this.getRepository = function() {
+	return this.repository;
+    };
+
+
+    /* ISSUES */
+    this.search = function(term, state) {
+	state = state || 'open';
+	var url= [
+	    'http://github.com/api/v2',
+	    this.getFormat(),
+	    'issues/search',
+	    this.getUsername(),
+	    this.getRepository(),
+	    state,
+	    term
+	].join('/');
+
+	app.log(url);
+	var issues = axiom.HTTP.get(url);
+	return issues.issues;
+    };
+
+    this.getIssues = function(state) {
+	state = state || 'open';
+	var url= [
+	    'http://github.com/api/v2',
+	    this.getFormat(),
+	    'issues/list',
+	    this.getUsername(),
+	    this.getRepository(),
+	    state
+	].join('/');
+
+	var issues = axiom.HTTP.get(url);
+
+	return issues.issues;
+    };
+
+    this.getIssue = function(number) {
+	var url= [
+	    'http://github.com/api/v2',
+	    this.getFormat(),
+	    'issues/show',
+	    this.getUsername(),
+	    this.getRepository(),
+	    number
+	].join('/');
+
+	var issues = axiom.HTTP.get(url);
+
+	return issues.issue;
+    };
+
+    this.createIssue = function(title, body) {
+	var url= [
+	    'http://github.com/api/v2',
+	    this.getFormat(),
+	    'issues/open',
+	    this.getUsername(),
+	    this.getRepository()
+	].join('/');
+
+	if (!(this.getUsername() && this.getToken() && title)) {
+	    throw "Required data does not exist. Please make sure you've specified a username, API token and title.";
+	}
+
+	var post_data = {
+	    login: this.getUsername(),
+	    token: this.getToken(),
+	    title: title
 	};
 
-	this.issues = function(state) {
-	    state = state || 'all';
-	    return [
-		'http://github.com/api/v2',
-		this.getFormat(),
-		'issues/list',
-		this.getUsername(),
-		this.getRepository(),
-		state].join('/');
-	};
+	if (body) {
+	    post_data.body = body;
+	}
 
-	return this;
+	try {
+	    var content = axiom.HTTP.post(url, post_data);
+	    return content.issue;
+	} catch (e) {
+	    app.log("Error: There was a problem with the server.");
+	    return null;
+	}
+    };
+
+
+    /* USERS */
+    this.getUser = function(username) {
+	var url= [
+	    'http://github.com/api/v2',
+	    this.getFormat(),
+	    'user/show',
+	    username
+	].join('/');
+	
+	var content = axiom.HTTP.get(url);
+	return content.user;
     };
 
     return this;
